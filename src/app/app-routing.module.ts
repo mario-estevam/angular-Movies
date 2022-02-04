0d8958a8411bed7c71aeaf93c85b5eeeadfcc43d
mario-estevam <mario.neto47@gmail.com>
2022-02-04 10:56:56 -0300

    aditional table

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
+import { CategoriaReadComponent } from './components/views/categoria/categoria-read/categoria-read.component';
 import { HomeComponent } from './components/template/home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  
+
   {path:'',
-  component:HomeComponent}
+  component:HomeComponent},
+  {path:'categorias',
+  component:CategoriaReadComponent}
+
 
 ];
 
